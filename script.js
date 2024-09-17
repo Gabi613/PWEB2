455c184862f0a930fab3af21c554c935d3ab8e0a
Gabi613 <agas5@aluno.ifal.edu.br>
2024-09-17 14:36:52 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function addrow (){
+    let tabela = document.querySelector('')
+    let row = document.createElement('tr')
+    let col01 = document.createElement('td')
+    let col02 = document.createElement('td')
+    col01.textContent = 'João'
+    col02.textContent = '25'
+    row.appendChild(col01)
+    row.appendChild(col02)
+    tabela.appendChild(row)
+}
